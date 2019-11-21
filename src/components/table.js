a993165676519275349f79dce4fe36e05b8543cc
truta <tpie2451@scs.ubbcluj.ro>
2019-11-21 15:36:52 +0200

    filter not working at all

--- src/components/table.js
+++ src/components/table.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import DataTable from 'react-data-table-component';
-import { Container } from '@material-ui/core';
- 
-const data = [{ id: 1, name: 'Handle', year: '1982' }];
+import { Container, Button } from '@material-ui/core';
+import { IconButton } from 'material-ui';
+
+const data = [{ id: 1, name: 'Crypto', year: '2019', semester:'1'},
+            { id: 2, name: 'VR', year: '2019', semester:'1' },
+            { id: 3, name: 'PPD', year: '2019', semester:'1' },
+            { id: 4, name: 'LFTC', year: '2019', semester:'1' },
+            { id: 5, name: 'Mobile', year: '2019', semester:'1' }];
+
+var searchString = '';
+var searchString1 = '';
+
 const columns = [
   {
     name: 'Name',
@@ -15,16 +24,54 @@ const columns = [
     sortable: true,
     right: true,
   },
+  {
+    name: 'Semester',
+    selector: 'semester',
+    sortable: true,
+  },
 ];
 
 export default class TableNew extends React.Component {
+  handleChange(e) {
+    searchString = e.target.value;
+  };
+
+  handleChange1(e) {
+    searchString1 = e.target.value;
+  };
+
+  activeClick(){
+    if(searchString.length > 0){
+      if (searchString1.length > 0){
+        data = data.filter(l => {
+          return l.year.toLowerCase().match( searchString ) && 
+                 l.semester.toLocaleLowerCase().match( searchString1 );
+      });
+      }
+    }
+  }
+
   render(){
 
+    searchString = searchString.trim().toLowerCase();
+    searchString1 = searchString1.trim().toLowerCase();
+
+
+    if(searchString.length > 0){
+      if (searchString1.length > 0){
+        data = data.filter(l => {
+          return l.year.toLowerCase().match( searchString );
+      });
+      }
+    }
+
   return (
-    <Container>
-      <h1>Input year:</h1>
-      <input type="text" name="year"></input>
-      <button type="submit">Filter</button>
+    <Container style={{width:"30vw"}}>
+      <p>Input year:</p>
+      <input type="text" name={searchString} style={{width:"5vw"}} onChange={this.handleChange} placeholder="Type here"></input>
+      <p>Input semester:</p>
+      <input type="text" name={searchString1} style={{width:"5vw"}} onChange={this.handleChange1} placeholder="Type here"></input>
+      <Button type="submit" onClick={this.activeClick()}>Filter</Button>
       
       <DataTable
         title="Subjects"
