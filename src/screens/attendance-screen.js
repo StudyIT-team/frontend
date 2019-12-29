a39781791a3bb26162a2eba50bfaa642627dcdf7
Gerlinde Christine <gerlindeciocea@yahoo.com>
2019-12-29 21:46:47 +0200

    added two excel like grids (attendance and grades)

--- src/screens/attendance-screen.js
+++ src/screens/attendance-screen.js
@@ -13,20 +13,42 @@ const columns = [
     { key: "lab7", name: "Laborator 7", editable: true },
   ];
   
-  const rows = [
-    { name: "Achim Diana-Nicoleta", lab1: 10, lab2: 10 }
+  const rows_grades = [
+    { name: "Achim Diana-Nicoleta", lab1: 10, lab2: 10 },
+    { name: "Ardelian Alexandra Ioana", lab1: 10, lab2: 10 },
+    { name: "Cardaș Andra Mălina", lab1: 10, lab2: 10, lab5: 10 },
+    { name: "Ciocea Christine-Gerlinde", lab1: 10, lab2: 10 },
+    { name: "Truța Patricia Georgiana", lab1: 10, lab2: 10 },
   ];
 
-export default class AttendanceSheet extends React.Component {
-    state = { rows };
+  const rows_attendance = [
+    { name: "Achim Diana-Nicoleta", lab1: "Present", lab2: "Present", lab3: "Present", lab4: "Present", lab5: "Present", lab6: "Present", lab7: "Present" },
+    { name: "Ardelian Alexandra Ioana", lab1: "Present", lab2: "Present", lab3: "Present", lab4: "Present", lab5: "Present", lab6: "Present", lab7: "Present" },
+    { name: "Cardaș Andra Mălina", lab1: "Present", lab2: "Present", lab3: "Present", lab4: "Present", lab5: "Present", lab6: "Present", lab7: "Present" },
+    { name: "Ciocea Christine-Gerlinde", lab1: "Present", lab2: "Absent", lab3: "Present", lab4: "Present", lab5: "Absent", lab6: "Present", lab7: "Present" },
+    { name: "Truța Patricia Georgiana",lab1: "Present", lab2: "Present", lab3: "Present", lab4: "Present", lab5: "Present", lab6: "Present", lab7: "Absent"},
+  ]
 
-    onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+export default class AttendanceSheet extends React.Component {
+    state = { rows_grades, rows_attendance };
+
+    onGridRowsUpdatedGrades = ({ fromRow, toRow, updated }) => {
         this.setState(state => {
-            const rows = state.rows.slice();
+            const rows = state.rows_grades.slice();
             for (let i = fromRow; i <= toRow; i++) {
-                rows[i] = { ...rows[i], ...updated };
+                rows_grades[i] = { ...rows_grades[i], ...updated };
             }
-            return { rows };
+            return { rows_grades };
+        });
+    };
+
+    onGridRowsUpdatedAttendance = ({ fromRow, toRow, updated }) => {
+        this.setState(state => {
+            const rows = state.rows_attendance.slice();
+            for (let i = fromRow; i <= toRow; i++) {
+                rows_attendance[i] = { ...rows_attendance[i], ...updated };
+            }
+            return { rows_attendance };
         });
     };
 
@@ -34,11 +56,19 @@ export default class AttendanceSheet extends React.Component {
         return (
             <Container maxWidth="l">
                 <ReactDataGrid
-                columns = {columns}
-                rowGetter={i => this.state.rows[i]}
-                rowsCount={3}
-                onGridRowsUpdated={this.onGridRowsUpdated}
-                enableCellSelect={true}
+                    columns = {columns}
+                    rowGetter={i => this.state.rows_attendance[i]}
+                    rowsCount={this.state.rows_attendance.length}
+                    onGridRowsUpdated={this.onGridRowsUpdatedAttendance}
+                    enableCellSelect={true}
+                />
+
+                <ReactDataGrid
+                    columns = {columns}
+                    rowGetter={i => this.state.rows_grades[i]}
+                    rowsCount={this.state.rows_grades.length}
+                    onGridRowsUpdated={this.onGridRowsUpdatedGrades}
+                    enableCellSelect={true}
                 />
             </Container>
         );
